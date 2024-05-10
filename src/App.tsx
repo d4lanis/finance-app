d82c62b20fc050a7b4761fc74c24f38c59ec539a
Daniel Alanis <daniel.alanis.hdz@gmail.com>
2024-05-09 23:42:54 -0600

    test github actions on path /

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App() {
     <ThemeProvider value={theme}>
       <Router>
         <Routes>
+          <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="register" element={<Register />} />
           <Route path="forgot-password" element={<ForgotPassword />} />
