a6e3733325c984f128c92ca4c7c1efceb4471cbc
Daniel Alanis <daniel.alanis.hdz@gmail.com>
2024-05-13 22:07:59 -0600

    remove base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import million from "million/compiler";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [million.vite({ auto: true }), react()],
-  base: "/<REPO>/"
+  plugins: [million.vite({ auto: true }), react()]
 });
